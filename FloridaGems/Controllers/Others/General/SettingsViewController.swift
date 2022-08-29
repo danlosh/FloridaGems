1da66ab622d14f32a24c66d9c6422c5ad2aee3b0
Daniel Loshkarev <81983874+danlosh@users.noreply.github.com>
2022-08-29 15:39:59 -0400

    Settings button added

--- FloridaGems/Controllers/Others/General/SettingsViewController.swift
+++ FloridaGems/Controllers/Others/General/SettingsViewController.swift
@@ -5,26 +5,28 @@
 //  Created by Daniel Loshkarev on 8/22/22.
 //
 
-import UIKit
 
+import SafariServices
+import UIKit
 
 struct SettingCellModel {
     let title: String
     let handler: (() -> Void)
 }
 
-/// View controller to show profile settings
-
+/// View Controller to show user settings
 final class SettingsViewController: UIViewController {
 
     private let tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        let tableView = UITableView(frame: .zero,
+                                    style: .grouped)
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
         return tableView
     }()
-    
-    private var data = [SettingCellModel]()
-    
+
+    private var data = [[SettingCellModel]]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureModels()
@@ -32,44 +34,136 @@ final class SettingsViewController: UIViewController {
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
-
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
-    
+
     private func configureModels() {
-        
-        let section = [
+        data.append([
+            SettingCellModel(title: "Edit Profile") { [weak self] in
+                self?.didTapEditProfile()
+            },
+            SettingCellModel(title: "Invite Friends") { [weak self] in
+                self?.didTapInviteFriends()
+            },
+            SettingCellModel(title: "Save Original Posts") { [weak self] in
+                self?.didTapSaveOriginalPosts()
+            }
+        ])
+
+
+        data.append([
+            SettingCellModel(title: "Terms of Service") { [weak self] in
+                self?.openURL(type: .terms)
+            },
+            SettingCellModel(title: "Privacy Policy") { [weak self] in
+                self?.openURL(type: .privacy)
+            },
+            SettingCellModel(title: "Help / Feedback") { [weak self] in
+                self?.openURL(type: .help)
+            }
+        ])
+
+        data.append([
             SettingCellModel(title: "Log Out") { [weak self] in
                 self?.didTapLogOut()
             }
-        ]
-        data.append(section)
+        ])
+    }
+
+    enum SettingsURLType {
+        case terms, privacy, help
+    }
+
+    private func openURL(type: SettingsURLType) {
+        let urlString: String
+        switch type {
+        case .terms: urlString = "https://help.instagram.com/581066165581870"
+        case .privacy: urlString = "https://help.instagram.com/519522125107875?helpref=page_content"
+        case .help: urlString = "https://help.instagram.com/"
+        }
+
+        guard let url = URL(string: urlString) else {
+            return
+        }
+
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+
+    private func didTapSaveOriginalPosts() {
+
+    }
+
+    private func didTapInviteFriends() {
+        // Show share sheet to invite friends
+    }
+
+    private func didTapEditProfile() {
+
     }
 
     private func didTapLogOut() {
         
+        let actionsheet = UIAlertController(title: "Log Out", message: "Are you sure you want to log out?", preferredStyle: .actionSheet)
+        actionsheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionsheet.addAction(UIAlertAction(title: "Confirm", style: .destructive, handler: { _ in
+            
+            AuthManager.shared.logOut { success in
+                DispatchQueue.main.async {
+                    if success {
+                        
+                        // present log in
+                        
+                        let loginVC = LoginViewController()
+                        loginVC.modalPresentationStyle = .fullScreen
+                        self.present(loginVC, animated: true) {
+                            self.navigationController?.popToRootViewController(animated: false)
+                            self.tabBarController?.selectedIndex = 0
+                        }
+                    }
+                    else {
+                        // error occurred
+                        
+                        fatalError("Could not log out user")
+                    }
+                }
+            }
+        }))
+        
+        actionsheet.popoverPresentationController?.sourceView = tableView
+        actionsheet.popoverPresentationController?.sourceRect = tableView.bounds
+        
+        present(actionsheet, animated: true)
     }
-}
 
-extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return data.count
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data[section].count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-          cell.textLabel?.text = data[indexPath.section][indexPath.row].title
-          cell.accessoryType = .disclosureIndicator
-          return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        data[indexPath.section][indexPath.row].handler()
-    }
+    
 }
+        
+        
+        
+        
+    extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return data.count
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return data[section].count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = data[indexPath.section][indexPath.row].title
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        }
+
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            data[indexPath.section][indexPath.row].handler()
+        }
+    }
